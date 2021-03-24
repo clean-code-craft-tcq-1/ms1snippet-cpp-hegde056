93d64127f75053622ed8ebd0d83b3108f214960d
Sudarshan Hegde <suduhegde01@gmail.com>
2021-03-24 10:26:40 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -4,3 +4,5 @@ bool _give_me_a_good_name(double value, double nextValue, double maxDelta);
 bool validateSOCreadings(double* values, int numOfValues);
 
 bool validateCurrentreadings(double* values, int numOfValues);
+
+bool validateReadings(double* values, int numOfValues , double maxDelta);
