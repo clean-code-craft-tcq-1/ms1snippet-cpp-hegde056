8f62f8bcc10a531c8ce5e56a8f00c0dd96657e5a
Sudarshan Hegde <suduhegde01@gmail.com>
2021-03-25 21:01:40 +0530

    *handles nullptr and empty array input

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,8 +1,10 @@
 
-bool _give_me_a_good_name(double value, double nextValue, double maxDelta);
+bool isValueDiffAccepted(double value, double nextValue, double maxDelta);
+
+bool checkValidInput(double* values, int numOfValues);
 
 bool validateSOCreadings(double* values, int numOfValues);
 
 bool validateCurrentreadings(double* values, int numOfValues);
 
-bool validateReadings(double* values, int numOfValues , double maxDelta);
+bool validateReadings(double* values, int numOfValues, double maxDelta);
